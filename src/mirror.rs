85d347aa33ae1cc3d1ba00b03668ea0e443afeb3
aelmir <adnane.elmir1@gmail.com>
2025-08-01 17:24:59 +0100

    t

--- src/mirror.rs
+++ src/mirror.rs
@@ -1,4 +1,7 @@
+use crate::models::DownloadConfig;
 
 pub async fn mirror_site(config: &DownloadConfig) -> Result<(), Box<dyn std::error::Error>> {
     // Recursive site download implementation
+    todo!()
+
 }
