bf517201f46dd6aaceac78bbc87c37d76fdf7054
Sphinex0 <adnane.elmir1@gmail.com>
2025-08-07 10:49:06 +0100

    clean

--- src/mirror.rs
+++
@@ -1,7 +0,0 @@
-use crate::models::DownloadConfig;
-
-pub async fn mirror_site(config: &DownloadConfig) -> Result<(), Box<dyn std::error::Error>> {
-    // Recursive site download implementation
-    todo!()
-
-}
